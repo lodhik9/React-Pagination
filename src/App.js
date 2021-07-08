79934841071518c0178ea7050f8524aefbf222b9
Muhammad Khan Lodhi <lodhik9@gmail.com>
2021-07-08 09:42:51 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import React, { useState } from 'react';
+import JsonData from './MOCK_DATA.json';
+import ReactPaginate from 'react-paginate';
 
 function App() {
+  const [users, setUsers] = useState(JsonData.slice(0, 51));
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const userPerPage = 10;
+  const pageVisited = pageNumber * userPerPage;
+
+  const pageCount = Math.ceil(users.length / userPerPage);
+  const pageChange = ({ selected }) => {
+    setPageNumber(selected);
+  }
+
+  const displayUsers = users.slice(pageVisited, pageVisited + userPerPage)
+    .map((user) => {
+      return (
+        <div className="user">
+          <h3>{user.firstName}</h3>
+          <h3>{user.lastName}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      );
+    })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {displayUsers}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={pageChange}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
   );
 }
